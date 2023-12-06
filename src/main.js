e8775039428a687214586c86fd072d7662104896
Angela Ku <anyaku@ucsc.edu>
2023-12-06 00:57:38 -0800

    Change scale config

--- src/main.js
+++ src/main.js
@@ -45,7 +45,10 @@ let config = {
     },
     width: 800,
     height: 600,
-    scale: {autoCenter: Phaser.Scale.CENTER_BOTH},
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
     scene: [ Menu, Credits, Manual, Play ],
 }
 
